bc44be8d80a2d6f898275797e26004174422ab30
Esley <esley.2011@gmail.com>
2024-02-07 11:00:26 -0300

    Estamos no caminho certo!

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,17 @@ const maxRecords = 151
 const limit = 10
 let offset = 0;
 
+class PokemonDetail {
+    number;
+    name;
+    type;
+    types = [];
+    photo;
+    weight;
+    height;
+    expBase;
+}
+
 function convertPokemonToLi(pokemon) {
     return `
         <a onclick="paginaNova(${pokemon.number})" href="#">
@@ -31,6 +42,27 @@ function loadPokemonItens(offset, limit) {
 
 }
 
+//função para instanciar nosso pokemon detail
+function DetailPokemon(responseJson) {
+    const pokemonDetail = new PokemonDetail()
+    pokemonDetail.number = responseJson.id
+    pokemonDetail.name = responseJson.name
+
+    const types = responseJson.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemonDetail.types = types
+    pokemonDetail.type = type
+
+    pokemonDetail.photo = responseJson.sprites.other.dream_world.front_default
+
+    pokemonDetail.height = responseJson.height
+    pokemonDetail.weight = responseJson.weight
+    pokemonDetail.expBase = responseJson.base_experience
+
+    return pokemonDetail
+}
+
 const screenDetail = document.querySelector('.screenDetail')
 const fundo = document.querySelector('.content')
 
@@ -38,12 +70,112 @@ const fundo = document.querySelector('.content')
 function paginaNova(identifyPoke) {
     
     const identify = identifyPoke;
+    const url = `https://pokeapi.co/api/v2/pokemon/${identify}`
     console.log(identify);
     
     screenDetail.classList.add("opacidade");
     fundo.classList.add("opacidade0");
+   
+    fetch(url)
+    .then((response) => response.json())
+    .then(
+        function(responseJson) {
+            let pokemonDetail = new PokemonDetail()
+            pokemonDetail.number = responseJson.id
+            pokemonDetail.name = responseJson.name
 
+            let types = responseJson.types.map((typeSlot) => typeSlot.type.name)
+            let [type] = types
+
+            pokemonDetail.types = types
+            pokemonDetail.type = type
+
+            pokemonDetail.photo = responseJson.sprites.other.dream_world.front_default
+
+            pokemonDetail.height = responseJson.height
+            pokemonDetail.weight = responseJson.weight
+            pokemonDetail.expBase = responseJson.base_experience
+
+            return pokemonDetail
+        }
+    )
+    .then((pokemonDetail) => {
+        console.log(pokemonDetail)
+        inserirHtml(pokemonDetail);
+    })
+
+   
 } 
+
+//função para inserir o HTML
+function inserirHtml(pokemonDetail) {
+    screenDetail.innerHTML = `
+    <header>
+        <div class="icons">
+            <a href="#" onclick="voltar()">
+                <img src="assets/img/seta-esquerda.png" alt="seta para voltar a página principal">
+            </a>
+            <img src="assets/img/coracao.png" alt="coração">
+        </div>
+        <div class="infoPokemon">
+            <h1 class="namePokemon">${pokemonDetail.name}</h1>
+            <span id="element" class="numberPokemon">#${pokemonDetail.number}</span>
+        </div>
+        <ol class="typesPokemon">
+            <li>Grass</li>
+            <li>Poison</li>
+        </ol>
+        <img class="imagem" height="400px" src="${pokemonDetail.photo}" alt="oi">
+    </header>
+    
+    <footer>
+        <ul class="menuDetail">
+            <a href="detailPokemon.html">
+                <li style="color: black; border-bottom: 1px solid black;">About</li>
+            </a>
+            <a href="#">
+                <li>Base Stats</li>
+            </a>
+            <a href="#">
+                <li>Evolution</li>
+            </a>
+            <a href="#">
+                <li>Moves</li>
+            </a>
+        </ul>
+        <div class="Detail">
+            <ul class="title">
+                <li>
+                    <span>Experiência base:</span>
+                </li>
+                <li>
+                    <span>height:</span>
+                </li>
+                <li>
+                    <span>Weight:</span>
+                </li>
+                <li>
+                    <span>Abilities:</span>
+                </li>
+            </ul>
+            <ul class="result">
+                <li>
+                    <span>${pokemonDetail.expBase}</span>
+                </li>
+                <li>
+                    <span>${pokemonDetail.height}</span>
+                </li>
+                <li>
+                    <span>${pokemonDetail.weight}</span>
+                </li>
+                <li>
+                    <span>teste</span>
+                </li>
+            </ul>
+        </div>
+    </footer>
+    `
+}
 
 //click sumir detalhes
 function voltar() {
